c4184d764ed1e61d89b6cd6ab1c32888313bcf42
Arsensio <100180501+Arsensio@users.noreply.github.com>
2022-06-20 09:01:11 +0600

    initial commit

---
+++ src/main/java/com/example/postemployee/HelloServlet.java
@@ -0,0 +1,40 @@
+package com.example.postemployee;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+        String a =  request.getParameter("name");
+        String b =request.getParameter("possiotion");
+
+        PrintWriter writer =    response.getWriter();
+        writer.write("Name = "+a +" & "+ " Possition = "+b);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String a =  req.getParameter("name");
+        String b =req.getParameter("possiotion");
+
+        PrintWriter writer =    resp.getWriter();
+        writer.write("Name = "+a +" & "+ " Possition = "+b);
+    }
+
+    public void destroy() {
+    }
+}
---
+++ target/postEmployee-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,18 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+
+<form action="hello-servlet" method="POST">
+    <label for="name">Name</label>
+    <input name = "name" id="name">
+    <label for="possition">Job Possition</label>
+    <input name = "possiotion" id="possition">
+    <button>Submit</button>
+</form>
+
+</body>
+</html>
